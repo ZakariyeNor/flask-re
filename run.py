159edc8d78bb66977cea40b4ecc53f4d26e1d323
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-12-01 10:31:49 +0000

    totall data

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ if os.path.exists("env.py"):
     
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -33,8 +34,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
